2bc60393de18e505b260eba57cca56cb0804858d
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2022-03-15 07:14:55 +0100

    Update

--- client/.cmake/vcpkg.cmake
+++ client/.cmake/vcpkg.cmake
@@ -1,26 +1,18 @@
-# Submodules Configuration
-
-#find_package(Git QUIET)
-#if(GIT_FOUND AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git")
-#    option(GIT_SUBMODULE "Check submodules during build" ON)
-#    if(GIT_SUBMODULE)
-#        message(STATUS "Updating git submodules...")
-#        execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
-#                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-#                        RESULT_VARIABLE GIT_SUBMOD_RESULT)
-#        if(NOT GIT_SUBMOD_RESULT EQUAL "0")
-#            message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
-#        endif()
-#    endif()
-#endif()
-
-# Vcpkg Toolchain Configuration
-
 set(VCPKG_TOOLCHAIN_FILE "${CMAKE_CURRENT_SOURCE_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake")
-
-if(NOT EXISTS ${VCPKG_TOOLCHAIN_FILE})
-    message(FATAL_ERROR "The vcpkg submodules was not downloaded! GIT_SUBMODULE was turned off or failed. Please update submodules and try again.")
+    
+if(EXISTS ${VCPKG_TOOLCHAIN_FILE})    
+    message(STATUS "Using vcpkg toolchain")
+    set(CMAKE_TOOLCHAIN_FILE ${VCPKG_TOOLCHAIN_FILE} CACHE STRING "Vcpkg toolchain file")
 endif()
 
-message(STATUS "Using vcpkg toolchain")
-set(CMAKE_TOOLCHAIN_FILE ${VCPKG_TOOLCHAIN_FILE} CACHE STRING "Vcpkg toolchain file")
+#file(TIMESTAMP "${CMAKE_CURRENT_SOURCE_DIR}/vcpkg.json" vcpkg_json_timestamp)
+#
+#if(VCPKG_JSON_TIMESTAMP_PREV STREQUAL vcpkg_json_timestamp)
+#    set(VCPKG_MANIFEST_INSTALL OFF CACHE STRING "" FORCE)
+#else()
+#    set(VCPKG_MANIFEST_INSTALL ON CACHE STRING "" FORCE)
+#endif()
+#
+#set(VCPKG_JSON_TIMESTAMP_PREV "${vcpkg_json_timestamp}" CACHE STRING "" FORCE)
+#
+# Controllare il timestamp dei registri
