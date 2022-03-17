bfcfad24748807d4eae13f7067dc6f4ad36c5daf
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2022-03-17 11:53:53 +0100

    Update

--- client/client/src/client.c
+++ client/client/src/client.c
@@ -1,3 +1,8 @@
-﻿int main(int argc, const char** argv) {
+﻿#include "buracchi/libcommon/utilities/try.h"
+
+int main(int argc, const char** argv) {
+	try(1, 0, fail);
 	return 0;
+fail:
+	return 1;
 }
