434792cd254b29ecf227a6d2f1dfbe000d01044c
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2022-06-01 18:04:38 +0200

    Update client

--- client/cinema-management-service/src/booking.c
+++ client/cinema-management-service/src/booking.c
@@ -1,16 +1,11 @@
 #include "booking.h"
 
-#include <stddef.h>
-
 #include "cms-statement.h"
 
 extern bool cms_get_all_cinema(cms_t cms, struct cms_get_all_cinema_response** response) {
 	struct result_bitmap result_bitmap[5] = {
-		{ offsetof(struct cms_get_all_cinema_response, result),			sizeof(struct cms_cinema) },
-		{ offsetof(struct cms_cinema, id),								sizeof(((struct cms_cinema*)0)->id) },
-		{ offsetof(struct cms_cinema, address),							sizeof(((struct cms_cinema*)0)->address) },
-		{ offsetof(struct cms_cinema, opening_time),					sizeof(((struct cms_cinema*)0)->opening_time) },
-		{ offsetof(struct cms_cinema, closing_time),					sizeof(((struct cms_cinema*)0)->closing_time) }
+		CMS_RESULT_BITMAP_INFO(struct cms_get_all_cinema_response, result[0]),
+		CMS_RESULT_BITMAP_INFO(struct cms_cinema, id, address, opening_time, closing_time)
 	};
 	return cms_stmt_execute(cms, GET_ALL_CINEMA, NULL, response, result_bitmap);
 }
@@ -18,16 +13,8 @@ extern bool cms_get_all_cinema(cms_t cms, struct cms_get_all_cinema_response** r
 extern bool cms_get_cinema_screenings(cms_t cms, struct cms_get_cinema_screenings_request request, struct cms_get_cinema_screenings_response** response) {
 	struct request_param request_params[1] = { { &request.cinema_id,	sizeof(request.cinema_id) } };
 	struct result_bitmap result_bitmap[10] = {
-		{ offsetof(struct cms_get_cinema_screenings_response, result),	sizeof(struct cms_screening) },
-		{ offsetof(struct cms_screening, date),							sizeof(((struct cms_screening*)0)->date) },
-		{ offsetof(struct cms_screening, time),							sizeof(((struct cms_screening*)0)->time) },
-		{ offsetof(struct cms_screening, cinema_id),					sizeof(((struct cms_screening*)0)->cinema_id) },
-		{ offsetof(struct cms_screening, hall_id),						sizeof(((struct cms_screening*)0)->hall_id) },
-		{ offsetof(struct cms_screening, price),						sizeof(((struct cms_screening*)0)->price) },
-		{ offsetof(struct cms_screening, film_name),					sizeof(((struct cms_screening*)0)->film_name) },
-		{ offsetof(struct cms_screening, running_time),					sizeof(((struct cms_screening*)0)->running_time) },
-		{ offsetof(struct cms_screening, film_studio),					sizeof(((struct cms_screening*)0)->film_studio) },
-		{ offsetof(struct cms_screening, cast),							sizeof(((struct cms_screening*)0)->cast) }
+		CMS_RESULT_BITMAP_INFO(struct cms_get_cinema_screenings_response, result[0]),
+		CMS_RESULT_BITMAP_INFO(struct cms_screening, date, time, cinema_id, hall_id, price, film_name, running_time, film_studio, cast)
 	};
 	return cms_stmt_execute(cms, GET_CINEMA_SCREENINGS, request_params, response, result_bitmap);
 }
@@ -35,10 +22,8 @@ extern bool cms_get_cinema_screenings(cms_t cms, struct cms_get_cinema_screening
 extern bool cms_get_cinema_halls(cms_t cms, struct cms_get_cinema_halls_request request, struct cms_get_cinema_halls_response** response) {
 	struct request_param request_params[1] = { { &request.cinema_id,	sizeof(request.cinema_id) } };
 	struct result_bitmap result_bitmap[4] = {
-		{ offsetof(struct cms_get_cinema_halls_response, result),		sizeof(struct cms_hall_info) },
-		{ offsetof(struct cms_hall_info, id),							sizeof(((struct cms_hall_info*)0)->id) },
-		{ offsetof(struct cms_hall_info, num_rows),						sizeof(((struct cms_hall_info*)0)->num_rows) },
-		{ offsetof(struct cms_hall_info, num_cols),						sizeof(((struct cms_hall_info*)0)->num_cols) }
+		CMS_RESULT_BITMAP_INFO(struct cms_get_cinema_halls_response, result[0]),
+		CMS_RESULT_BITMAP_INFO(struct cms_hall_info, id, num_rows, num_cols)
 	};
 	return cms_stmt_execute(cms, GET_HALLS, request_params, response, result_bitmap);
 }
@@ -51,9 +36,8 @@ extern bool cms_get_available_seats(cms_t cms, struct cms_get_available_seats_re
 		{ &request.start_time,	sizeof(request.start_time) }
 	};
 	struct result_bitmap result_bitmap[3] = {
-		{ offsetof(struct cms_get_available_seats_response, result),	sizeof(struct cms_seat) },
-		{ offsetof(struct cms_seat, row),								sizeof(((struct cms_seat*)0)->row) },
-		{ offsetof(struct cms_seat, number),							sizeof(((struct cms_seat*)0)->number) }
+		CMS_RESULT_BITMAP_INFO(struct cms_get_available_seats_response, result[0]),
+		CMS_RESULT_BITMAP_INFO(struct cms_seat, row, number)
 	};
 	return cms_stmt_execute(cms, GET_AVALILABLE_SEATS, request_params, response, result_bitmap);
 }
@@ -72,8 +56,8 @@ extern bool cms_book_seat(cms_t cms, struct cms_book_seat_request request, struc
 		{ &request.security_code,	sizeof(request.security_code) }
 	};
 	struct result_bitmap result_bitmap[3] = {
-		{ offsetof(struct cms_book_seat_response, result),				sizeof(struct cms_book_seat_result) },
-		{ offsetof(struct cms_book_seat_result, booking_code),			sizeof(((struct cms_book_seat_result*)0)->booking_code) },
+		CMS_RESULT_BITMAP_INFO(struct cms_book_seat_response, result[0]),
+		CMS_RESULT_BITMAP_INFO(struct cms_book_seat_result, booking_code)
 	};
 	return cms_stmt_execute(cms, BOOK_SEAT, request_params, response, result_bitmap);
 }
